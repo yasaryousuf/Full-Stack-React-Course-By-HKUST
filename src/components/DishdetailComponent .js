8c462b772764fe4487d2d1b4e9a2030ef6e6b448
yasaryousuf <yousuf802@gmail.com>
2019-10-20 14:27:37 +0600

    actions redux

--- src/components/DishdetailComponent .js
+++ src/components/DishdetailComponent .js
@@ -138,7 +138,7 @@ function RenderDish({ dish }) {
   );
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, dishId }) {
   if (comments != null) {
     return (
       <div className="col-12 col-md-5 m-1">
@@ -156,7 +156,7 @@ function RenderComments({ comments }) {
             </li>
           </ul>
         ))}
-        <CommentForm />
+        <CommentForm dishId={dishId} addComment={addComment} />
       </div>
     );
   } else return <div />;
@@ -178,7 +178,10 @@ const DishDetailComponent = props => (
     </div>
     <div className="row">
       <RenderDish dish={props.dish} />
-      <RenderComments comments={props.comments} />
+      <RenderComments comments={props.comments}
+        addComment={props.addComment}
+        dishId={props.dish.id}
+      />
     </div>
   </div>
 );
